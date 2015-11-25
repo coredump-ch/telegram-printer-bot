61742f25c707578b729e3494bb4a4ae8a64461c0
Jannis Grimm <jannis@gje.ch>
2015-11-25 12:57:17 +0100

    Warn when not in group chat

--- server.js
+++ server.js
@@ -8,19 +8,25 @@ var bot = new TelegramBot(config.token, {polling: true});
 var printers = new Map();
 
 bot.onText(/^\/start(@CoredumpPrinterBot)?$/, function(message, match) {
+  var messageId = message.message_id;
   var chatId = message.chat.id;
-  bot.sendMessage(chatId, 'Hey!');
+  bot.sendMessage(chatId, 'Hey!', {'reply_to_message_id': messageId});
+  checkGroupChat(message.chat);
 });
 
 bot.onText(/^\/help(@CoredumpPrinterBot)?$/, function(message, match) {
   var chatId = message.chat.id;
   bot.sendMessage(chatId, 'Ich verwalte Reservierungen für unseren 3D-Drucker.\n\nUm eine Reservierung zu tätigen: /reservetime\nUm kommende Reservierungen anzuzeigen: /reservations\nUm eine Reservierung zu löschen: Mit /reservetime die Dauer auf 0 setzen.\nFremde Zeiten überschreiben ist möglich – ich bin nicht da, um Konflikte zu lösen.');
+  checkGroupChat(message.chat);
 });
 
 bot.onText(/^\/reservations(@CoredumpPrinterBot)?$/, function(message) {
   var chatId = message.chat.id;
   
   cleanReservations();
+  if (!checkGroupChat(message.chat)) {
+    return;
+  }
   
   if (!printers.has(chatId)) {
     bot.sendMessage(chatId, 'Nächste Reservationen: keine.');
@@ -41,6 +47,9 @@ bot.onText(/^\/reservations(@CoredumpPrinterBot)?$/, function(message) {
 bot.onText(/^\/reservetime(@CoredumpPrinterBot)? ([^ ]* [^ ]*) (\d{1,2})/, function(message, match) {
   var messageId = message.message_id;
   var chatId = message.chat.id;
+  if (!checkGroupChat(message.chat)) {
+    return;
+  }
   
   var fromTime = match[2];
   if (!moment(Date.parse(fromTime)).isValid()) {
@@ -62,6 +71,7 @@ bot.onText(/^\/reservetime(@CoredumpPrinterBot)?( .*)?$/, function(message, matc
   
   if (!match[2] || !/[^ ]* [^ ]* \d{1,2}/.test(match[2])) {
     bot.sendMessage(chatId, 'Bitte Kommando in der Form /reservetime YYYY-MM-DD HH:mm HH angeben, z. B. /reservetime 2017-02-12 18:15 6 um den Drucker am 12. Februar 2017 ab 18:15 Uhr für 6 Stunden zu reservieren.', {'reply_to_message_id': messageId});
+    checkGroupChat(message.chat);
   }
 });
 
@@ -99,4 +109,15 @@ function cleanReservations() {
       printers.delete(printerId);
     }
   });
+}
+
+function checkGroupChat(chat) {
+  if (chat.type != 'private') {
+    return true;
+  }
+  
+  setTimeout(function () {
+    bot.sendMessage(chat.id, 'Ich funktioniere nur in Gruppenchats, weil ich eine eigene Reservierungsliste für jeden Gruppenchat verwalte.');
+  }, 1000);
+  return false;
 }
